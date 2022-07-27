a2579a3fb3e55d4bfac36da8d2bfd088e86481fe
rahimedu <edwin.duche82@gmail.com>
2022-07-26 23:57:05 -0400

    initmessageofapp.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Product from './Pages/product'
 import productDetails from './Pages/productDetails'
 import Home from './Pages/home' 
 import NotFound from './Pages/nofound'
-import {BrowserRouter ,Switch,Route} from 'react-router-dom'
-import store from './Api/store'
-import {Provider} from 'react-redux'
 
 
 function App() {
