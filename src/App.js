6f345a65c534f961ec82622a6ab282fce9fbeb1e
rahimedu <edwin.duche82@gmail.com>
2022-07-26 23:54:07 -0400

    addcomponentsinapp.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import NavComponent from './Pages/Navbar';
 import Cart from './Pages/cart'
 import Product from './Pages/product'
-import productDetails from './Pages/productDetails'
-import Home from './Pages/home' 
-import NotFound from './Pages/nofound'
+
 import {BrowserRouter ,Switch,Route} from 'react-router-dom'
 import store from './Api/store'
 import {Provider} from 'react-redux'
