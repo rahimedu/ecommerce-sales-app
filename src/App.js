c44cfac48990062ed8a743fb8d7f3688d7df9569
rahimedu <edwin.duche82@gmail.com>
2022-07-27 00:00:54 -0400

    restoredcomponentsinapp.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import NavComponent from './Pages/Navbar';
+import Cart from './Pages/cart'
+import Product from './Pages/product'
+import productDetails from './Pages/productDetails'
+import Home from './Pages/home' 
+import NotFound from './Pages/nofound'
+import {BrowserRouter ,Switch,Route} from 'react-router-dom'
+import store from './Api/store'
+import {Provider} from 'react-redux'
+
 
 function App() {
   return (
