d9e1731b5ae4d01e9b3e9e4eb6f06d48c39eae33
rahimedu <edwin.duche82@gmail.com>
2022-07-28 13:02:27 -0400

    Updates

---
+++ precache-manifest.a1b7817c2aeed073ce9c031cd113ca7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ada38c20369dffbd6a9dba4d1bc3e4",
+    "url": "/ReactEcommerce/index.html"
+  },
+  {
+    "revision": "eef21e0e16ce86179682",
+    "url": "/ReactEcommerce/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "316a9e8a5b20acef0c09",
+    "url": "/ReactEcommerce/static/css/main.379e6641.chunk.css"
+  },
+  {
+    "revision": "eef21e0e16ce86179682",
+    "url": "/ReactEcommerce/static/js/2.ad866052.chunk.js"
+  },
+  {
+    "revision": "704a91b005260117b5b4bae209198db9",
+    "url": "/ReactEcommerce/static/js/2.ad866052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "316a9e8a5b20acef0c09",
+    "url": "/ReactEcommerce/static/js/main.dc382e02.chunk.js"
+  },
+  {
+    "revision": "2c6b83e81f0b9754b21d",
+    "url": "/ReactEcommerce/static/js/runtime-main.f582e885.js"
+  }
+]);
